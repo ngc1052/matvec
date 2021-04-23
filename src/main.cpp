d73e5d6de4d9fd0c854a1c9d8f8560a8e67a1e35
ngc1052 <robin.oberfrank@gmail.com>
2021-04-23 14:06:00 +0200

    Setup, hello world

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#define CL_HPP_ENABLE_EXCEPTIONS
+#define CL_HPP_MINIMUM_OPENCL_VERSION 100
+#define CL_HPP_TARGET_OPENCL_VERSION 120
+
+#include <iostream>
+#include <CL/cl2.hpp>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    return 0;
+}
